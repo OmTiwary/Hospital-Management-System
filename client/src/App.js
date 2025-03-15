f4a34d9c1d2e9d14f2a7a31bd9531e3285e0f885
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-15 08:46:42 +0530

    Sidebar with app.js working

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Dashboard from './components/Dashboard/Dashboard'
+import Appointment from './components/Appointment/Appointment'
+import Campaign from './components/Campaign/Campaign'
+import Medecine from './components/Medicine/Medicine'
+import Patients from './components/Patients/Patient'
+import Invoices from './components/Invoices/Invoice'
+import Payments from './components/Payments/Payment'
+import Profile from './components/Profile/Profile'
+import Reception from './components/Reception/Reception'
+import Service from './components/Services/Service'
+import Settings from './components/Settings/Setting'
+import Doctors from './components/Doctors/Doctors'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <BrowserRouter>
+    <div className="app-container">
+      <Sidebar />
+      <div className="content-container">
+        <Routes>
+        <Route path="/" element= {<Dashboard />} />
+        <Route path="/appointment" element= {<Appointment />} />
+        <Route path="/campaign" element= {<Campaign />} />
+        <Route path="/doctors" element= {<Doctors />} />
+        <Route path="/invoices" element= {<Invoices />} />
+        <Route path="/medecine" element= {<Medecine />} />
+        <Route path="/patients" element= {<Patients />} />
+        <Route path="/payments" element= {<Payments />} />
+        <Route path="/profile" element= {<Profile />} />
+        <Route path="/reception" element= {<Reception />} />
+        <Route path="/service" element= {<Service />} />
+        <Route path="/settings" element= {<Settings />} />
+        </Routes>
+      </div>
     </div>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import './Sidebar.css'
+import {Link} from "react-router-dom";
+
+import profile from '../asset/profile.png'
+import logo from '../asset/logo.avif'
+
+export default function Sidebar() {
+    return (
+        <>
+            <div className="sidebar">
+                <div className="sidebar-header">
+                    <img src={logo} alt="logo" />
+                    <h2>Xeno Health</h2>
+                </div>
+                <ul className="sidebar-links">
+                    <h4>
+                        <span>Operations</span>
+                        <div className="divider"></div>
+                    </h4>
+                    <li>
+                        <Link to= '/'>
+                        <span><i class="fa-solid fa-house-user" style={{ color: "#63E6BE" }}></i></span>
+                        Dashboard
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/patients'>
+                        <span>
+                            <i class="fa-regular fa-user" style={{ color: "#63E6BE" }}>
+                            </i>
+                        </span>
+                        Patients
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/reception'>
+                        <span>
+                            <i class="fa-solid fa-bell-concierge" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Receptions
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/doctors'>
+                        <span>
+                            <i class="fa-solid fa-user-doctor" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Doctors
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/appointment'>
+                        <span>
+                            <i class="fa-solid fa-calendar-check" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Appointment
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/payments'>
+                        <span>
+                            <i class="fa-brands fa-paypal" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Payments
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/invoices'>
+                        <span>
+                            <i class="fa-solid fa-file-invoice" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Invoices
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/services'>
+                        <span>
+                            <i class="fa-solid fa-truck-medical" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Services
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/medicine'>
+                        <span>
+                            <i class="fa-solid fa-syringe" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Medicine
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/campaign'>
+                        <span>
+                            <i class="fa-solid fa-campground" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Campaign
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/settings'>
+                        <span>
+                            <i class="fa-solid fa-gear" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Settings
+                        </Link>
+                    </li>
+                    <h4>
+                        <span>Account</span>
+                        <div className="divider"></div>
+                    </h4>
+                    <li>
+                        <Link to='/profile'>
+                        <span>
+                            <i class="fa-solid fa-address-card" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Profile
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/settings'>
+                        <span>
+                            <i class="fa-solid fa-gear" style={{ color: "#63E6BE" }}></i>
+                        </span>
+                        Settings
+                        </Link>
+                    </li>
+                </ul>
+                <div className="user-account">
+                    <div className="user-profile">
+                    <img src={profile} alt="" />
+                        <div className="user-detail">
+                            <h3>Ayush Mishra</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
