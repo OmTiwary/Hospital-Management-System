816cfb3a922b9f75e6397a82ef6f08996b35bc8d
Om Tiwary <143310925+OmTiwary@users.noreply.github.com>
2025-03-25 22:15:41 +0530

    Working Landing page is created

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Reception from './components/Reception/Reception'
 import Service from './components/Services/Service'
 import Settings from './components/Settings/Setting'
 import Doctors from './components/Doctors/Doctors'
+import Landing from './authentication/Landing'
 
 
 function App() {
@@ -20,22 +21,36 @@ function App() {
     <>
     <BrowserRouter>
     <div className="app-container">
-      <Sidebar />
-      <div className="content-container">
-        <Routes>
-        <Route path="/" element= {<Dashboard />} />
-        <Route path="/appointment" element= {<Appointment />} />
-        <Route path="/campaign" element= {<Campaign />} />
-        <Route path="/doctors" element= {<Doctors />} />
-        <Route path="/invoices" element= {<Invoices />} />
-        <Route path="/medicine" element= {<Medecine />} />
-        <Route path="/payments" element= {<Payments />} />
-        <Route path="/profile" element= {<Profile />} />
-        <Route path="/reception" element= {<Reception />} />
-        <Route path="/service" element= {<Service />} />
-        <Route path="/settings" element= {<Settings />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/" element={
+          <>
+            <Sidebar />
+            <div className="content-container">
+              <Dashboard />
+            </div>
+          </>
+        } />
+        <Route path="/*" element={
+          <>
+            <Sidebar />
+            <div className="content-container">
+              <Routes>
+                <Route path="/appointment" element={<Appointment />} />
+                <Route path="/campaign" element={<Campaign />} />
+                <Route path="/doctors" element={<Doctors />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/medicine" element={<Medecine />} />
+                <Route path="/payments" element={<Payments />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/reception" element={<Reception />} />
+                <Route path="/service" element={<Service />} />
+                <Route path="/settings" element={<Settings />} />
+              </Routes>
+            </div>
+          </>
+        } />
+      </Routes>
     </div>
     </BrowserRouter>
     </>
