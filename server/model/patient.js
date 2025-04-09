37dc5a1be41d063177e58d4cee1730a6f6e5f6cb
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-04-09 15:14:16 +0530

    Fully functional client and admin panel now only remaing of adding some dummy data to the profile component

---
+++ server/model/patient.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    phone: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    confirmPassword: {type: String, required: true}
+})
+
+module.exports = mongoose.model("appointment", userSchema)
